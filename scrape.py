3e30cec47fa8748ff8b42c6771967d13986de849
marcelabarella <marcela.barella@hotmail.com>
2025-01-20 21:43:40 -0300

    add detail_top_scorer function

--- scrape.py
+++ scrape.py
@@ -82,7 +82,55 @@ def top_scorers():
     top_scorers.Club = top_scorers.Club.apply(lambda mc: 'Manchester City' if 'Manchester City' in mc else mc)
     top_scorers.Club = top_scorers.Club.apply(lambda mu: 'Manchester United' if 'Manchester United' in mu else mu)
     top_scorers.Club = top_scorers.Club.apply(lambda bha: 'Brighton & Hove Albion' if 'Brighton & Hove Albion' in bha else bha)
-    print(top_scorers)
+
     return top_scorers
 
-top_scorers()
+def detail_top_scorer():
+    # page link of the site were data will be extracted
+    url = 'https://www.worldfootball.net/goalgetter/eng-premier-league-2023-2024/'
+    # list to store the headers
+    headers = []
+    # makes a get request that returns the html
+    page = requests.get(url)
+
+    # used to clean and sort throught the html content to get the needed data
+    soup = BeautifulSoup(page.text, 'html.parser')
+    # find the content in this html tag, using class_ cause class is a key world
+    table = soup.find('table', class_='standard_tabelle')
+
+    # finds all html tags with th header
+    for th in table.find_all('th'): 
+        # gets the text content
+        title = th.text
+        # appends the title to the headers
+        headers.append(title)
+
+    #creates dataframes with the headers
+    detail_top_scorer = pd.DataFrame(columns=headers)
+
+    # finds all the html tags with tr
+    for tr in table.find_all('tr')[1:]:
+        row_data = tr.find_all('td')
+        row = [td.text for td in row_data]
+        lenght = len(detail_top_scorer)
+        # gets them by row and saves to the league table dataframe
+        detail_top_scorer.loc[lenght] = row
+
+    detail_top_scorer = detail_top_scorer.drop([''], axis=1)
+    
+    detail_top_scorer.Team = detail_top_scorer.Team.str.replace('\n\n', '')
+
+    detail_top_scorer['Penalty'] = detail_top_scorer['Goals (Penalty)'].str.split().str[-1:].str.join(' ')
+    detail_top_scorer['Penalty'] = detail_top_scorer['Penalty'].str.replace('(', '')
+    detail_top_scorer['Penalty'] = detail_top_scorer['Penalty'].str.replace(')', '')
+    
+    detail_top_scorer['Goals (Penalty)'] = detail_top_scorer['Goals (Penalty)'].str.split().str[0].str.join('')
+
+    detail_top_scorer.rename(columns= {'Goals (Penalty)': 'Goals'}, inplace=True)
+
+    detail_top_scorer = detail_top_scorer.drop(['#'], axis=1)
+
+    print(detail_top_scorer)
+    return detail_top_scorer
+
+detail_top_scorer()
